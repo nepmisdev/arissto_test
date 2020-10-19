e2ae2e1259f947604abf5e33315ffc317cfeaaa2
chinwanyee <45148249+chinwanyee@users.noreply.github.com>
2020-10-19 22:06:51 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -800,7 +800,6 @@ function subPlan() {
 			<span class="plan-words">RM1 Office Buddies Plan<br/><span class="plan-type"><?php
 			echo $data['Membership_Type'];
 			?></span><br /></span></div><?php
-			
 	    	} //else echo "123";
 		}
 	}
