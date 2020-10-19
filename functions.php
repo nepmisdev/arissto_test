7c37d38ec2da6fe25a7a7de08b1866c99908ae1a
chinwanyee <45148249+chinwanyee@users.noreply.github.com>
2020-10-19 21:57:57 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -794,11 +794,11 @@ function subPlan() {
 			<span class="plan-words">RM1 Home Coffee Plan<br/><span class="plan-type">
 			<?php echo $data['Membership_Type']; ?>
 			</span><br/></span></div>
-		<?php } else if (strpos($Membership_Type, $ssplan) !== FALSE) {?>
+		<?php } elseif (strpos($Membership_Type, $ssplan) !== FALSE) {?>
 			<div class="plan-detail-even">
         		<img src="http://arissto.com/test/wp-content/uploads/2020/10/MachineMasterFrontm1.png" class="plan-image"/>
 			<span class="plan-words">RM1 Office Buddies Plan<br/><span class="plan-type"><?php
-			echo "1234"; //$data['Membership_Type'];
+			echo $data['Membership_Type'];
 			?></span><br /></span></div><?php
 			
 	    	} //else echo "123";
