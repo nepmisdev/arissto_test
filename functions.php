2a8e57567d75ecf3d50b04c5a71f7139c8c1e3b4
chinwanyee <45148249+chinwanyee@users.noreply.github.com>
2020-10-19 18:40:36 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -788,13 +788,13 @@ function subPlan() {
 		
 		if(!empty($array)) {
 			foreach($array as $data) {
-			if (strpos($mplan, $applan) !== FALSE) {?>
+			if (strpos($Membership_Type, $applan) !== FALSE) {?>
 			<div class="plan-detail-odd">
 			<img src="http://arissto.com/test/wp-content/uploads/2020/10/happy_maker_2.0.png"class="plan-image"/>
 			<span class="plan-words">RM1 Home Coffee Plan<br/><span class="plan-type">
 			<?php echo $data['Membership_Type']; ?>
 			</span><br/></span></div>
-		<?php } else if (strpos($mplan, $ssplan) !== FALSE ) {?>
+		<?php } else if (strpos($Membership_Type, $ssplan) !== FALSE ) {?>
 			<div class="plan-detail-even">
         		<img src="http://arissto.com/test/wp-content/uploads/2020/10/MachineMasterFrontm1.png" class="plan-image"/>
 			<span class="plan-words">RM1 Office Buddies Plan<br /><span class="plan-type"><?php
